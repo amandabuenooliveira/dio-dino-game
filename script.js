e8cc3828e03efb0df88a978c40cb5bb0a573734d
Amanda Cristina Bueno Miranda de Oliveira <amanda.bueno@olxbr.net>
2021-10-03 13:19:49 -0300

    exclamacao

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ function createCactus() {
       // Game over
       clearInterval(leftTimer);
       isGameOver = true;
-      document.body.innerHTML = '<h1 class="game-over">ACABOOOOOOU</h1>';
+      document.body.innerHTML = '<h1 class="game-over">ACABOOOOOOU!!!</h1>';
     } else {
       cactusPosition -= 10;
       cactus.style.left = cactusPosition + 'px';
